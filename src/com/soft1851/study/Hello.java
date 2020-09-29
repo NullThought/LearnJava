e362deb54f71f5249ce9f94fb5c647bb769fe8c9
XueSrc <2947896925@qq.com>
2020-09-29 22:50:52 +0800

    hello java

---
+++ src/com/soft1851/study/Hello.java
@@ -0,0 +1,13 @@
+package com.soft1851.study;
+
+public class Hello {
+
+        public static void main(String[] args) {
+            // 向屏幕输出文本:
+            System.out.println("Hello, java!");
+        /* 多行注释开始
+        注释内容
+        注释结束 */
+        }
+    } // class定义结束
+
