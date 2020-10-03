fd15fe87c71b983f32cb9fcc09a10f42c058a83e
XueSrc <2947896925@qq.com>
2020-10-03 21:55:32 +0800

    do-whole

---
+++ src/com/softjava/DoWhile.java
@@ -0,0 +1,13 @@
+package com.softjava;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int sum = 0;
+        int n = 1;
+        do {
+            sum = sum + n;
+            n ++;
+        } while (n <= 100);
+        System.out.println(sum);
+    }
+}
